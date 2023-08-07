e32c2449d89534aa01514c3633980782b34033f2
pattharock <ritvik1609@gmail.com>
2023-08-07 17:03:14 +0800

    fibonacci recursion

---
+++ src/smallproblems/Fib1.java
@@ -0,0 +1,14 @@
+package smallproblems;
+
+public class Fib1 {
+    private static int fib1(int n) {
+        if (n < 2){
+            return n;
+        }
+        return fib1(n - 1) + fib1(n - 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Fib(9): " + Integer.toString(fib1(9)));
+    }
+}
